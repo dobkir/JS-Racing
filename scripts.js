3c2c5f1b1e924a3516ceec0bd900563b89919d7b
dobkir <dobkir@gmail.com>
2021-06-24 21:00:54 +0400

    Added music, stop the game when cars intersected, restart the game

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
-// 'use strict';
+'use strict';
 
 //======================= Global Variables =======================//
+const MAX_ENEMY_CARS = 7;
 
 const score = document.querySelector('.score'),
   start = document.querySelector('.start'),
@@ -8,6 +9,18 @@ const score = document.querySelector('.score'),
   userCar = document.createElement('div');
 
 userCar.classList.add('userCar');
+
+/* 
+// Add music to the game (method 'embed')
+const music = document.createElement('embed');
+music.src = 'music.mp3';
+music.classList.add('visually-hidden');
+*/
+
+// Add music to the game (object 'audio')
+const music = new Audio('music.mp3');
+
+
 
 const keys = {
   ArrowUp: false,
@@ -43,11 +56,31 @@ document.addEventListener('keyup', stopRun);
  * This function returns the number of elements that can be placed on the page.
  **/
 function getQuantityElementElements(heightElement) {
-  return document.documentElement.clientHeight / heightElement + 1;
+  return document.documentElement.clientHeight / heightElement;
+}
+
+function getRandomEnemyCar(max) {
+  return Math.floor((Math.random() * max) + 1);
 }
 
 function startGame() {
   start.classList.add('hide');
+  gameArea.innerHTML = '';
+
+
+  /*  Add music to the game (method 'embed')
+  // Adding music to the game
+  
+  document.body.append(music);
+  
+  // Removing music from the game
+    
+  setTimeout(() => {
+    music.remove();
+  }, 4000);
+  */
+
+  music.play();
 
   /**
    * Lines are used to animate the movement of the car
@@ -67,15 +100,23 @@ function startGame() {
     const enemyCar = document.createElement('div');
     enemyCar.classList.add('enemyCar');
     enemyCar.y = -100 * setting.traffic * (i + 1);
-    enemyCar.style.left = Math.trunc(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+    enemyCar.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
     enemyCar.style.top = enemyCar.y + 'px';
-    enemyCar.style.background = 'transparent url(./image/enemy.png) center / cover no-repeat';
+    enemyCar.style.background = `
+    transparent 
+    url(./image/enemy${getRandomEnemyCar(MAX_ENEMY_CARS)}.png) 
+    center / cover 
+    no-repeat`;
     gameArea.appendChild(enemyCar);
   }
 
+  setting.score = 0;
   setting.start = true;
 
   gameArea.appendChild(userCar);
+  userCar.style.left = '125px';
+  userCar.style.top = 'auto';
+  userCar.style.bottom = '10px';
   setting.x = userCar.offsetLeft;
   setting.y = userCar.offsetTop;
   requestAnimationFrame(playGame);
@@ -84,8 +125,11 @@ function startGame() {
 function playGame() {
 
   if (setting.start) {
+    setting.score += setting.speed;
+    score.innerHTML = 'SCORE:<br>' + setting.score;
     moveRoad();
     moveEnemy();
+
     if (keys.ArrowLeft && setting.x > 0) {
       setting.x -= setting.speed;
     }
@@ -107,13 +151,17 @@ function playGame() {
 }
 
 function startRun(event) {
-  event.preventDefault();
-  keys[event.key] = true;
+  if (keys.hasOwnProperty(event.key)) {
+    event.preventDefault();
+    keys[event.key] = true;
+  }
 }
 
 function stopRun(event) {
-  event.preventDefault();
-  keys[event.key] = false;
+  if (keys.hasOwnProperty(event.key)) {
+    event.preventDefault();
+    keys[event.key] = false;
+  }
 }
 
 function moveRoad() {
@@ -123,7 +171,7 @@ function moveRoad() {
     line.y += setting.speed;
     line.style.top = line.y + 'px';
 
-    if (line.y > document.documentElement.clientHeight) {
+    if (line.y >= document.documentElement.clientHeight) {
       line.y = -100;
     }
   });
@@ -133,12 +181,35 @@ function moveEnemy() {
   let enemyCars = document.querySelectorAll('.enemyCar');
 
   enemyCars.forEach(function (enemyCar) {
+    /**
+     * elem.getBoundingClientRect() returns window coordinates for a minimal
+     * rectangle that encloses elem as an object of built-in DOMRect class.
+     * Used this method so that cars cannot drive through each other.
+     **/
+    let userCarRect = userCar.getBoundingClientRect();
+    let enemyCarRect = enemyCar.getBoundingClientRect();
+
+    /**
+     * When rectangles of cars intersected, 
+     * the game should be stopped.
+     **/
+    if (userCarRect.top <= enemyCarRect.bottom &&
+      userCarRect.right >= enemyCarRect.left &&
+      userCarRect.left <= enemyCarRect.right &&
+      userCarRect.bottom >= enemyCarRect.top) {
+      setting.start = false;
+      music.load();
+      console.warn('WTF?!');
+      start.classList.remove('hide');
+      score.style.top = start.offsetHeight;
+    }
+
     enemyCar.y += setting.speed / 2;
     enemyCar.style.top = enemyCar.y + 'px';
 
-    if (enemyCar.y > document.documentElement.clientHeight) {
+    if (enemyCar.y >= document.documentElement.clientHeight) {
       enemyCar.y = -100 * setting.traffic;
-      enemyCar.style.left = Math.trunc(Math.random() * gameArea.offsetWidth) + 'px';
+      enemyCar.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
     }
   });
 }
