893390491e9d453170aee237e3e562d7f390360b
dobkir <dobkir@gmail.com>
2021-06-25 23:10:57 +0400

    Added the difficulty of the game. Founded the way to increases along with scores

--- scripts.js
+++ scripts.js
@@ -2,11 +2,13 @@
 
 //======================= Global Variables =======================//
 const MAX_ENEMY_CARS = 7;
+const HEIGHT_ELEM = 100;
 
 const score = document.querySelector('.score'),
   start = document.querySelector('.start'),
   gameArea = document.querySelector('.gameArea'),
-  userCar = document.createElement('div');
+  userCar = document.createElement('div'),
+  btns = document.querySelectorAll('.btn');
 
 userCar.classList.add('userCar');
 
@@ -19,7 +21,7 @@ music.classList.add('visually-hidden');
 
 // Add music to the game (object 'audio')
 const music = new Audio('music.mp3');
-
+music.volume = 0.1;
 
 
 const keys = {
@@ -35,6 +37,8 @@ const setting = {
   speed: 3,
   traffic: 3
 };
+
+let startSpeed = 0;  // When the difficulty increases along with scores.
 
 //=================== End of Global Variables ===================//
 
@@ -56,17 +60,52 @@ document.addEventListener('keyup', stopRun);
  * This function returns the number of elements that can be placed on the page.
  **/
 function getQuantityElementElements(heightElement) {
-  return document.documentElement.clientHeight / heightElement;
+  return gameArea.offsetHeight / heightElement + 1;
 }
 
 function getRandomEnemyCar(max) {
   return Math.floor((Math.random() * max) + 1);
 }
 
-function startGame() {
-  start.classList.add('hide');
-  gameArea.innerHTML = '';
+function changeLevel(lvl) {
+  switch (lvl) {
+    case '1':
+      setting.traffic = 4;
+      setting.speed = 3;
+      break;
+    case '2':
+      setting.traffic = 3;
+      setting.speed = 6;
+      break;
+    case '3':
+      setting.traffic = 2;
+      setting.speed = 8;
+      break;
+  }
 
+  startSpeed = setting.speed;  // When the difficulty increases along with scores.
+}
+
+//================= Start the game =================//
+
+function startGame(event) {
+  //=================== The choice of the game difficulty level ===================//
+
+  const target = event.target;
+  if (!target.classList.contains('btn')) return;
+  const levelGame = target.dataset.levelGame;
+  changeLevel(levelGame);
+
+  // After the choice of the difficulty, buttons must be blocked
+  btns.forEach(btn => btn.disabled = true);
+
+  //=================== End of the choice of the game difficulty level ===================//
+
+  gameArea.style.minHeight = Math.floor((document.documentElement.clientHeight - HEIGHT_ELEM) / HEIGHT_ELEM) * HEIGHT_ELEM;
+
+  start.classList.add('hide');
+
+  gameArea.innerHTML = '';
 
   /*  Add music to the game (method 'embed')
   // Adding music to the game
@@ -85,35 +124,36 @@ function startGame() {
   /**
    * Lines are used to animate the movement of the car
    **/
-  for (let i = 0; i < getQuantityElementElements(100); i++) {
+  for (let i = 0; i < getQuantityElementElements(HEIGHT_ELEM); i++) {
     const lines = document.createElement('div');
     lines.classList.add('lines');
-    lines.style.top = (i * 100) + 'px';
-    lines.y = i * 100;
-    gameArea.appendChild(lines);
+    lines.style.top = (i * HEIGHT_ELEM) + 'px';
+    lines.style.height = (HEIGHT_ELEM / 2) + 'px';
+    lines.y = i * HEIGHT_ELEM;
+    gameArea.append(lines);
   }
 
   /**
    * The traffic of enemy cars on the road
    **/
-  for (let i = 0; i < getQuantityElementElements(100 * setting.traffic); i++) {
+  for (let i = 0; i < getQuantityElementElements(HEIGHT_ELEM * setting.traffic); i++) {
     const enemyCar = document.createElement('div');
     enemyCar.classList.add('enemyCar');
-    enemyCar.y = -100 * setting.traffic * (i + 1);
+    enemyCar.y = -HEIGHT_ELEM * setting.traffic * (i + 1);
     enemyCar.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
     enemyCar.style.top = enemyCar.y + 'px';
     enemyCar.style.background = `
     transparent 
     url(./image/enemy${getRandomEnemyCar(MAX_ENEMY_CARS)}.png) 
-    center / cover 
+    center / contain
     no-repeat`;
-    gameArea.appendChild(enemyCar);
+    gameArea.append(enemyCar);
   }
 
   setting.score = 0;
   setting.start = true;
 
-  gameArea.appendChild(userCar);
+  gameArea.append(userCar);
   userCar.style.left = '125px';
   userCar.style.top = 'auto';
   userCar.style.bottom = '10px';
@@ -122,11 +162,16 @@ function startGame() {
   requestAnimationFrame(playGame);
 }
 
+//================= Play the game =================//
+
 function playGame() {
 
   if (setting.start) {
     setting.score += setting.speed;
     score.innerHTML = 'SCORE:<br>' + setting.score;
+
+    setting.speed = startSpeed + Math.floor(setting.score / 5000);  // When the difficulty increases along with scores.
+
     moveRoad();
     moveEnemy();
 
@@ -147,6 +192,11 @@ function playGame() {
     userCar.style.top = setting.y + 'px';
 
     requestAnimationFrame(playGame);
+  } else {
+    // After the end of the game, buttons of the difficulty choice must be unblocked
+    btns.forEach(btn => btn.disabled = true);
+    // And music must be reload.
+    music.load();
   }
 }
 
@@ -171,8 +221,8 @@ function moveRoad() {
     line.y += setting.speed;
     line.style.top = line.y + 'px';
 
-    if (line.y >= document.documentElement.clientHeight) {
-      line.y = -100;
+    if (line.y >= gameArea.offsetHeight) {
+      line.y = -HEIGHT_ELEM;
     }
   });
 }
@@ -198,17 +248,16 @@ function moveEnemy() {
       userCarRect.left <= enemyCarRect.right &&
       userCarRect.bottom >= enemyCarRect.top) {
       setting.start = false;
-      music.load();
+
       console.warn('WTF?!');
       start.classList.remove('hide');
-      score.style.top = start.offsetHeight;
     }
 
     enemyCar.y += setting.speed / 2;
     enemyCar.style.top = enemyCar.y + 'px';
 
-    if (enemyCar.y >= document.documentElement.clientHeight) {
-      enemyCar.y = -100 * setting.traffic;
+    if (enemyCar.y >= gameArea.offsetHeight) {
+      enemyCar.y = -HEIGHT_ELEM * setting.traffic;
       enemyCar.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
     }
   });
