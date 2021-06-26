d309fe5e6bdaa482eeee76620b913fa1ca03f961
dobkir <dobkir@gmail.com>
2021-06-26 23:52:29 +0400

    Updated the top panel with program prompts. Added pause and continue events.

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
 'use strict';
 
 //======================= Global Variables =======================//
-const MAX_ENEMY_CARS = 7;
+const MAX_BOT_CARS = 7;
 const HEIGHT_ELEM = 100;
 
-const score = document.querySelector('.score'),
-  start = document.querySelector('.start'),
+const inviteStart = document.querySelector('.system__invite--start'),
+  inviteGame = document.querySelector('.system__invite--game'),
+  inviteContinue = document.querySelector('.system__invite--continue'),
+  startPanel = document.querySelector('.panel__start'),
+  scorePanel = document.querySelector('.panel__score'),
+  gamePanel = document.querySelector('.panel__game'),
   gameArea = document.querySelector('.gameArea'),
-  userCar = document.createElement('div'),
-  btns = document.querySelectorAll('.btn');
+  buttons = document.querySelectorAll('.btn'),
+  gamePaused = document.querySelector('.game--paused'),
+  gameContinue = document.querySelector('.game--continue'),
+  gameMain = document.querySelector('.game--main'),
+  userCar = document.createElement('div');
 
 userCar.classList.add('userCar');
 
-/* 
-// Add music to the game (method 'embed')
-const music = document.createElement('embed');
-music.src = 'music.mp3';
-music.classList.add('visually-hidden');
-*/
-
 // Add music to the game (object 'audio')
 const music = new Audio('music.mp3');
-music.volume = 0.1;
+music.volume = 0.15;
 
 
 const keys = {
@@ -44,13 +44,7 @@ let startSpeed = 0;  // When the difficulty increases along with scores.
 
 
 
-//======================== Event Listeners =======================//
 
-start.addEventListener('click', startGame);
-document.addEventListener('keydown', startRun);
-document.addEventListener('keyup', stopRun);
-
-//==================== End of Event Listeners ===================//
 
 
 
@@ -63,8 +57,8 @@ function getQuantityElementElements(heightElement) {
   return gameArea.offsetHeight / heightElement + 1;
 }
 
-function getRandomEnemyCar(max) {
-  return Math.floor((Math.random() * max) + 1);
+function getRandomBotCar(max) {
+  return Math.floor(Math.random() * max + 1);
 }
 
 function changeLevel(lvl) {
@@ -97,29 +91,19 @@ function startGame(event) {
   changeLevel(levelGame);
 
   // After the choice of the difficulty, buttons must be blocked
-  btns.forEach(btn => btn.disabled = true);
+  // buttons.forEach(button => button.disabled = true);
 
   //=================== End of the choice of the game difficulty level ===================//
 
+  // Set game area height
   gameArea.style.minHeight = Math.floor((document.documentElement.clientHeight - HEIGHT_ELEM) / HEIGHT_ELEM) * HEIGHT_ELEM;
 
-  start.classList.add('hide');
+  inviteStart.classList.add('hide');
+  inviteGame.classList.remove('hide');
+  startPanel.classList.add('hide');
+  gamePanel.classList.remove('hide');
 
   gameArea.innerHTML = '';
-
-  /*  Add music to the game (method 'embed')
-  // Adding music to the game
-  
-  document.body.append(music);
-  
-  // Removing music from the game
-    
-  setTimeout(() => {
-    music.remove();
-  }, 4000);
-  */
-
-  music.play();
 
   /**
    * Lines are used to animate the movement of the car
@@ -127,8 +111,8 @@ function startGame(event) {
   for (let i = 0; i < getQuantityElementElements(HEIGHT_ELEM); i++) {
     const lines = document.createElement('div');
     lines.classList.add('lines');
-    lines.style.top = (i * HEIGHT_ELEM) + 'px';
-    lines.style.height = (HEIGHT_ELEM / 2) + 'px';
+    lines.style.top = i * HEIGHT_ELEM + 'px';
+    lines.style.height = HEIGHT_ELEM / 2 + 'px';
     lines.y = i * HEIGHT_ELEM;
     gameArea.append(lines);
   }
@@ -137,17 +121,17 @@ function startGame(event) {
    * The traffic of enemy cars on the road
    **/
   for (let i = 0; i < getQuantityElementElements(HEIGHT_ELEM * setting.traffic); i++) {
-    const enemyCar = document.createElement('div');
-    enemyCar.classList.add('enemyCar');
-    enemyCar.y = -HEIGHT_ELEM * setting.traffic * (i + 1);
-    enemyCar.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
-    enemyCar.style.top = enemyCar.y + 'px';
-    enemyCar.style.background = `
+    const botCar = document.createElement('div');
+    botCar.classList.add('botCar');
+    botCar.y = -HEIGHT_ELEM * setting.traffic * (i + 1);
+    botCar.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+    botCar.style.top = botCar.y + 'px';
+    botCar.style.background = `
     transparent 
-    url(./image/enemy${getRandomEnemyCar(MAX_ENEMY_CARS)}.png) 
+    url(./image/enemy${getRandomBotCar(MAX_BOT_CARS)}.png) 
     center / contain
     no-repeat`;
-    gameArea.append(enemyCar);
+    gameArea.append(botCar);
   }
 
   setting.score = 0;
@@ -162,18 +146,30 @@ function startGame(event) {
   requestAnimationFrame(playGame);
 }
 
+//================= The End of Start the game =================//
+
+
 //================= Play the game =================//
 
 function playGame() {
 
+
+  gamePaused.disabled = false;
+  gameContinue.disabled = true;
+
+  /**
+   * Add music to the game 
+  **/
+  music.play();
+
   if (setting.start) {
     setting.score += setting.speed;
-    score.innerHTML = 'SCORE:<br>' + setting.score;
+    scorePanel.innerHTML = 'SCORE:<br>' + setting.score;
 
     setting.speed = startSpeed + Math.floor(setting.score / 5000);  // When the difficulty increases along with scores.
 
     moveRoad();
-    moveEnemy();
+    moveBots();
 
     if (keys.ArrowLeft && setting.x > 0) {
       setting.x -= setting.speed;
@@ -193,12 +189,11 @@ function playGame() {
 
     requestAnimationFrame(playGame);
   } else {
-    // After the end of the game, buttons of the difficulty choice must be unblocked
-    btns.forEach(btn => btn.disabled = true);
-    // And music must be reload.
-    music.load();
+    stopGame()
   }
 }
+
+//================= The End of Play the game =================//
 
 function startRun(event) {
   if (keys.hasOwnProperty(event.key)) {
@@ -227,40 +222,94 @@ function moveRoad() {
   });
 }
 
-function moveEnemy() {
-  let enemyCars = document.querySelectorAll('.enemyCar');
+function moveBots() {
+  let botCars = document.querySelectorAll('.botCar');
 
-  enemyCars.forEach(function (enemyCar) {
+  botCars.forEach(function (botCar) {
     /**
      * elem.getBoundingClientRect() returns window coordinates for a minimal
      * rectangle that encloses elem as an object of built-in DOMRect class.
      * Used this method so that cars cannot drive through each other.
      **/
     let userCarRect = userCar.getBoundingClientRect();
-    let enemyCarRect = enemyCar.getBoundingClientRect();
+    let botCarRect = botCar.getBoundingClientRect();
 
     /**
      * When rectangles of cars intersected, 
      * the game should be stopped.
      **/
-    if (userCarRect.top <= enemyCarRect.bottom &&
-      userCarRect.right >= enemyCarRect.left &&
-      userCarRect.left <= enemyCarRect.right &&
-      userCarRect.bottom >= enemyCarRect.top) {
+    if (userCarRect.top <= botCarRect.bottom &&
+      userCarRect.right >= botCarRect.left &&
+      userCarRect.left <= botCarRect.right &&
+      userCarRect.bottom >= botCarRect.top) {
       setting.start = false;
 
       console.warn('WTF?!');
-      start.classList.remove('hide');
     }
 
-    enemyCar.y += setting.speed / 2;
-    enemyCar.style.top = enemyCar.y + 'px';
+    botCar.y += setting.speed / 2;
+    botCar.style.top = botCar.y + 'px';
 
-    if (enemyCar.y >= gameArea.offsetHeight) {
-      enemyCar.y = -HEIGHT_ELEM * setting.traffic;
-      enemyCar.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+    if (botCar.y >= gameArea.offsetHeight) {
+      botCar.y = -HEIGHT_ELEM * setting.traffic;
+      botCar.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
     }
   });
 }
 
+function pausedGame() {
+  gamePaused.disabled = true;
+  console.log(gamePaused.disabled);
+  gameContinue.disabled = false;
+  inviteGame.classList.add('hide');
+  inviteContinue.classList.remove('hide');
+  music.pause();
+  setting.start = false;
+  // requestAnimationFrame(playGame);
+}
+
+function continueGame() {
+  setting.start = true;
+  inviteGame.classList.remove('hide');
+  inviteContinue.classList.add('hide');
+  playGame();
+}
+
+function stopGame() {
+  // And music must be reload.
+  music.load();
+}
+
+// function stopGame() {
+//   // cancelAnimationFrame(startGame);
+//   // cancelAnimationFrame(playGame);
+//   setting.score = 0;
+//   setting.start = false;
+//   startSpeed = 0;
+//   inviteStart.classList.remove('hide');
+//   inviteGame.classList.add('hide');
+//   startPanel.classList.remove('hide');
+//   gamePanel.classList.add('hide');
+
+//   // After the end of the game, buttons of the difficulty choice must be unblocked
+//   buttons.forEach(btn => btn.disabled = false);
+//   // And music must be reload.
+//   music.load();
+
+//   gameArea.innerHTML = '';
+//   gameArea.style.minHeight = 0;
+// }
+
 //============= End of Handlers of Event Listeners =============//
+
+//======================== Event Listeners =======================//
+
+startPanel.addEventListener('click', startGame);
+document.addEventListener('keydown', startRun);
+document.addEventListener('keyup', stopRun);
+
+gamePaused.addEventListener('click', pausedGame);
+gameContinue.addEventListener('click', continueGame);
+gameMain.addEventListener('click', stopGame());
+
+//==================== End of Event Listeners ===================//
